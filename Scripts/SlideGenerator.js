ffa2888487ddeb741141f16bd943f6d514ec9044
CharifMah <charif25430@hotmail.fr>
2023-03-19 21:30:33 +0100

    added AfterEffect-SonyVegas carousel

--- Scripts/SlideGenerator.js
+++ Scripts/SlideGenerator.js
@@ -12,8 +12,10 @@ function GenerateSlides()
         const numImages = slider[i].dataset.count;
 
         GenerateButton(numImages,i,slider)
-
-        GenerateImage(numImages,i,slider,imagePath)
+        if (slider[i].dataset.folderpath != "AfterEffect-SonyVegas")
+            GenerateImage(numImages,i,slider,imagePath);
+        else
+            GenerateVideo(numImages,i,slider,imagePath)
     }
 }
 
@@ -52,6 +54,48 @@ function GenerateImage(numImages,i,slider,imagePath)
         divSlides.appendChild(div);
     }
 
+    slider[i].appendChild(divSlides);
+}
+
+function GenerateVideo(numVideos, i, slider, videoPath) {
+    // Crée un nouvel élément div pour les diapositives
+    const divSlides = document.createElement('div');
+    divSlides.className = "slides";
+
+    // Boucle pour générer les éléments div avec les vidéos
+    for (let index = 1; index <= numVideos; index++) {
+
+        // Crée un nouvel élément div avec un ID unique
+        const div = document.createElement('div');
+        div.id = 'slide-' + i + '-' + index;
+
+        // Crée un nouvel élément video avec l'attribut src correspondant à la vidéo
+        const video = document.createElement('video');
+        video.src = videoPath + index + '.mp4';
+        video.alt = 'Vidéo ' + index;
+
+        // Ajoute des sources vidéo pour chaque élément "video"
+        const source1 = document.createElement('source');
+        source1.src = videoPath + index + '.mp4';
+        source1.type = 'video/mp4';
+        video.appendChild(source1);
+
+        const source2 = document.createElement('source');
+        source2.src = videoPath + index + '.webm';
+        source2.type = 'video/webm';
+        video.appendChild(source2);
+
+        // Ajoute l'attribut "controls" pour permettre à l'utilisateur de contrôler la lecture de la vidéo
+        video.setAttribute("controls", "");
+
+        // Ajoute l'élément "video" à l'élément "div"
+        div.appendChild(video);
+
+        // Ajoute l'élément "div" au slides
+        divSlides.appendChild(div);
+    }
+
+    // Ajoute l'ensemble des diapositives générées à votre élément "slider"
     slider[i].appendChild(divSlides);
 }
 
